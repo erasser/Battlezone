005fb24ce42cc4f27f95d2dfbed0b6485a87ae31
Michal Grosser <m.grosser@seznam.cz>
2022-11-30 20:03:17 +0100

    Implements vehicle hovering

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,15 +2,54 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject vehicle1;
+    float _forwardPower;
+    float _sidePower;
+    public static GameObject actualVehicle;
+
     void Start()
     {
+
+        // Time.timeScale = .2f;
         
+        #if UNITY_EDITOR
+            UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));  // Don't auto-switch to the Game tab
+        #endif
+
+        actualVehicle = vehicle1;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            _forwardPower += .04f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _forwardPower = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _sidePower = -.8f;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _sidePower = .8f;
+        }
+        else
+            _sidePower = 0;
     }
+
+    void FixedUpdate()
+    {
+
+        // _rb.AddForce(_rb.transform.right * _forwardPower, ForceMode.Impulse);
+        // _rb.AddForce(_rb.transform.forward * _forwardPower, ForceMode.Impulse);
+        // _rb.AddForce(_rb.transform.right * _sidePower * 10000, ForceMode.Impulse);
+
+        // _rb.AddForce(_rb.transform.right * 80, ForceMode.Impulse);
+    }
+
+
 }
---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    readonly Vector3 _vector3Down = - Vector3.up;
+    Rigidbody _rb;
+    [Tooltip("Rozmezí houpání, tj. velikost výkyvu.")]
+    public float length = 20;       //    1
+    [Tooltip("Vyšší hmotnost vyžaduje vyšší sílu. Síla působí směrem nahoru.")]
+    public float strength = 60;     // 6000
+    [Tooltip("Tvrdost tlumiče.")]
+    public float dampening = 100;   // 6000
+    float _lastHitDist;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // gravity
+        if (Physics.Raycast(_rb.transform.position, _rb.transform.TransformDirection(_vector3Down), out var hit, length))
+        {
+            float forceAmount = HooksLawDampen(hit.distance);
+
+            // _rb.AddForceAtPosition(_rb.transform.up * forceAmount, _rb.transform.position);
+            _rb.AddForce(_rb.transform.up * forceAmount);
+        }
+        else
+            _lastHitDist = 1.1f * length;
+    }
+    
+    float HooksLawDampen(float hitDistance)
+    {
+        float forceAmount = strength * (length - hitDistance) + dampening * (_lastHitDist - hitDistance);
+        forceAmount = Mathf.Max(0, forceAmount);
+        _lastHitDist = hitDistance;
+
+        return forceAmount;
+    }
+}
